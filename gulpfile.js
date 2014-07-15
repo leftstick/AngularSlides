333ebea6d34c04309da7dcaec3dc97adb5dfd573
Howard.Zuo <leftstick@qq.com>
2014-07-15 17:47:03 +0800

    finish most parts of this slides

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,16 @@ var gulp = require('gulp');
 var connect = require('gulp-connect');
 
 gulp.task('default', function(cb) {
+
+    gulp.watch([
+        './index.html',
+        './resources/**/*'
+    ], function(event) {
+
+        gulp.src([event.path]).pipe(connect.reload());
+
+    });
+
     connect.server({
         port: 8080,
         livereload: true
