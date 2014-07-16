76c938a7426ed3eba7c149e7a262efaa87b651a2
Howard.Zuo <leftstick@qq.com>
2014-07-16 10:43:39 +0800

    add platform choosing feature while build the package

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,14 @@
 var gulp = require('gulp');
 var connect = require('gulp-connect');
 var clean = require('gulp-clean');
+var inquirer = require("inquirer");
 
-gulp.task('default', function (cb) {
+gulp.task('default', function(cb) {
 
     gulp.watch([
         './index.html',
         './resources/**/*'
-    ], function (event) {
+    ], function(event) {
 
         gulp.src([event.path]).pipe(connect.reload());
 
@@ -21,44 +22,56 @@ gulp.task('default', function (cb) {
     cb();
 });
 
-gulp.task('clean', function () {
+gulp.task('clean', function() {
     return gulp.src('./build', {
-            read: false
-        })
-        .pipe(clean({
-            force: true
-        }));
+        read: false
+    })
+    .pipe(clean({
+        force: true
+    }));
 });
 
 
-gulp.task('build', ['clean'], function (cb) {
+gulp.task('build', ['clean'], function(cb) {
 
+    inquirer.prompt([{
+            type: 'checkbox',
+            name: 'platforms',
+            message: 'Choose platforms you want to generate for',
+            default: ['win'],
+            choices: ['win', 'osx', 'linux32', 'linux64'],
+            validate: function(input) {
+                if (!input || input.length === 0) {
+                    return 'You have to choose at least one platform';
+                }
+                return true;
+            }
+        }], function(answer) {
 
-    var NwBuilder = require('node-webkit-builder');
-    var nw = new NwBuilder({
-        files: ['./**/*', '!./node_modules/**/*', '!./introduction/**/*'],
-        platforms: ['win'],
-        version: 'v0.9.2'
-    });
+        var NwBuilder = require('node-webkit-builder');
+        var nw = new NwBuilder({
+            files: ['./index.html',
+                './package.json',
+                './core/**/*',
+                './extensions/**/*',
+                './res/**/*',
+                './themes/**/*'],
+            platforms: answer.platforms,
+            version: 'v0.9.2'
+        });
 
-    // Log stuff you want
-    nw.on('log', console.log);
+        // Log stuff you want
+        nw.on('log', console.log);
 
-    // Build retruns a promise
-    nw.build().then(function () {
-        console.log('all done!');
-        cb();
-    }).catch(function (error) {
-        console.error(error);
-        cb(error);
-    });
+        // Build retruns a promise
+        nw.build().then(function() {
+                console.log('all done!');
+                cb();
+            }).catch(function(error) {
+            console.error(error);
+            cb(error);
+        });
 
-    // And supports callbacks
-    nw.build(function (err) {
-        if (err) {
-            console.log(err);
-            cb(err);
-        }
     });
 
 });
